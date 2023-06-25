de60287cf9424f6511a14912f29904a34ff60dcb
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-25 00:50:50 -0400

    change(languages): Register localeES and localeEN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEn from '@angular/common/locales/en';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/header/navbar/navbar.component';
+
+registerLocaleData(localeEs);
+registerLocaleData(localeEn);
 
 @NgModule({
   declarations: [
