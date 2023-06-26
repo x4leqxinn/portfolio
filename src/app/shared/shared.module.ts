623b1e47526d5aa2784005e5d2a6d17942b41f03
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-26 04:16:00 -0400

    add(module): shared module added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[NavbarComponent]
+})
+export class SharedModule { }
