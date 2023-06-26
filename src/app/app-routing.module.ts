5ee4fae5e76cc5d2cf5f3661657a1a2d8a627778
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-26 04:11:23 -0400

    change(routes): Main routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'home', 
+    loadChildren: () =>  import('./home/home.module').then(m => m.HomeModule), 
+  },
+  {
+    path: '**',
+    redirectTo : '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
