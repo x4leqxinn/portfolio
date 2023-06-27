d98798df1e28ab29f88d18d4a4be297c84ef8bbb
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-27 04:21:50 -0400

    add(styles): Tailwind default config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: false,
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: {
+          'main' : '#d42eff',
+          'main-100' : '#e699ff',
+          'main-200' : '#b61ee6',
+          'main-300' : '#8000b3'
+        },
+        background: {
+          'light': '#f5f5f5',
+          'dark': '#1e1e20'
+        },
+      }
+    },
+    fontFamily: {
+      exo: ['Exo 2'],
+    },
   },
   plugins: [],
 }
-
